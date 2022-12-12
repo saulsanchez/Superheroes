556ba036c7e1b79018119626c33b7e7d5074b037
saulsanchez <bluemonday.saul@gmail.com>
2022-12-12 17:07:38 +0100

    Upload project

---
+++ src/test/java/com/w2m/superheroes/SuperheroesApplicationTests.java
@@ -0,0 +1,14 @@
+package com.w2m.superheroes;
+
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+//@SpringBootTest
+class SuperheroesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
